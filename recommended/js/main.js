8bd0b64c5a28646cd6998889784f546ceec16525
XianFengWu1996 <shawnwu1996@hotmail.com>
2022-03-30 19:05:51 -0400

    remove new and recommend

--- recommended/js/main.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-    $('.open-bar').on("click",function(){
-        $('.mobile-nav').css('display', 'block');
-    });
-
-    $('.mobile-nav li i').on('click', function(){
-        $('.mobile-nav').css('display', 'none');
-    });
-});
